1f0a4973a93125d5a693b52ba8978dbbb7d3eaf0
jbeadles72 <72588338+jbeadles72@users.noreply.github.com>
2020-10-11 17:22:43 -0700

    Update Okta Script.ps1

--- Okta Script.ps1
+++ Okta Script.ps1
@@ -1,4 +1,4 @@
-﻿Connect-Okta "00DUBOLF7ebPyp57eAPXDK8kmRlibBNfwQzGx--fyj" https://dev-jakebeadles.okta.com
+﻿Connect-Okta "token" https://dev-jakebeadles.okta.com
 Get-OktaUser "me" 
 $profile = @{login = "xfreak360@gmail.com"; email = "xfreak360@gmail.com"; firstName = "Jake"; lastName = "Beadles"}
 $user = New-OktaUser @{profile = $profile}

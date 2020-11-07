dc79d2723b30f01921d0c10c27442bbb21995dba
jbeadles72 <72588338+jbeadles72@users.noreply.github.com>
2020-11-07 11:52:22 -0800

    Add files via upload

---
+++ Creating Bulk Users using a For Loop.ps1
@@ -0,0 +1,15 @@
+﻿Connect-Okta "token" url
+Get-OktaUser "me"
+function New-Users($numUsers) {
+    $now = Get-Date -Format "yyyyMMddHHmmss"
+    for ($i = 1; $i -le $numUsers; $i++) {
+        $profile = @{login="a$now$i@okta.com"; email="testuser$i@okta.com"; firstName="test"; lastName="ZExp$i"}
+        try {
+            $user = New-OktaUser @{profile = $profile; credentials = @{password = @{value = "Password123"}}} $false
+            Write-Host $i
+        } catch {
+            Get-Error $_
+        }
+    }
+}
+New-Users %number

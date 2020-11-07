2a6c8d56a9f1be626fa6dd62948e7bf88297bb0f
jbeadles72 <72588338+jbeadles72@users.noreply.github.com>
2020-11-07 11:54:22 -0800

    Add files via upload

---
+++ Filters and Pagination.ps1
@@ -0,0 +1,12 @@
+﻿Connect-Okta "token" url
+Get-OktaUser "me"
+$params = @{filter = 'status eq "STAGED"'}
+do {
+    $page = Get-OktaUsers @params
+    $users = $page.objects
+    foreach ($user in $users) {
+        # Add more properties here:
+        Write-Host $user.profile.login $user.profile.email
+    }
+    $params = @{url = $page.nextUrl}
+} while ($page.nextUrl)

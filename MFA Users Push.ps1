6b903c52798d0e7b4f59cbdae0d8420fe2ea338f
jbeadles72 <72588338+jbeadles72@users.noreply.github.com>
2020-11-14 11:00:23 -0800

    Add files via upload

---
+++ MFA Users Push.ps1
@@ -0,0 +1,33 @@
+﻿
+function Get-MfaUsers() {
+    $totalUsers = 0
+    $mfaUsers = @()
+    # for more filters, see https://developer.okta.com/docs/api/resources/users#list-users-with-a-filter
+    $params = @{filter = 'status eq "ACTIVE"'}
+    do {
+        $page = Get-OktaUsers @params
+        $users = $page.objects
+        foreach ($user in $users) {
+            $factors = Get-OktaFactors $user.id
+
+            $sms = $factors.where({$_.factorType -eq "sms"})
+            $push = $factors.where({$_.factorType -eq "push"})
+
+            $mfaUsers += [PSCustomObject]@{
+                id = $user.id
+                name = $user.profile.login
+                sms = $sms.factorType
+                sms_enrolled = $sms.created
+                sms_status = $sms.status
+                push = $push.factorType
+                push_enrolled = $push.created
+                push_status = $push.status
+            }
+        }
+        $totalUsers += $users.count
+        $params = @{url = $page.nextUrl}
+    } 
+    while ($page.nextUrl)
+    $mfaUsers | Export-Csv mfaUsers.csv -notype
+    "$totalUsers users found."
+    }
